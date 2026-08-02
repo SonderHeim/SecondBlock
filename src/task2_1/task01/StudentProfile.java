0f3140bbd72f92c1ea38e1d630291a38ea207d02
Reinhard <deuxexmachina1973@gmail.com>
2026-08-02 15:27:18 +0300

    Change named logic v2.

---
+++ src/task2_1/task01/StudentProfile.java
@@ -0,0 +1,31 @@
+package task2_1.task01;
+
+public class StudentProfile {
+    byte mark = 5;
+    short studentTicket = 127;
+    int birthDate = 1042005;
+    long phoneNumber = 79201242692L;
+    float averageMark = 4.5f;
+    double finalMark = 4.642;
+    boolean isPayment = true;
+    char firstLetterOfName = 'Д';
+
+    @Override
+    public String toString() {
+        return "StudentProfile{" +
+                "mark=" + mark +
+                ", studentTicket=" + studentTicket +
+                ", birthDate=" + birthDate +
+                ", phoneNumber=" + phoneNumber +
+                ", averageMark=" + averageMark +
+                ", finalMark=" + finalMark +
+                ", isPayment=" + isPayment +
+                ", firstLetterOfName=" + firstLetterOfName +
+                '}';
+    }
+
+    public static void main(String[] args) {
+        StudentProfile studentProfile = new StudentProfile();
+        System.out.println(studentProfile.toString());
+    }
+}
---
+++ src/task2_4/task04/GuessGame.java
@@ -0,0 +1,16 @@
+package task2_4.task04;
+
+public class GuessGame {
+
+    public static void main(String[] args) {
+        int secret = 42;
+        int guess = 15;
+
+        if (secret == guess)
+            System.out.println("Угадал!");
+        else if (guess < secret)
+            System.out.println("Меньше.");
+        else
+            System.out.println("Больше.");
+    }
+}
