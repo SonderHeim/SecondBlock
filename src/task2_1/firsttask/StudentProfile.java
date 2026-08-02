3b4c8e1b8385cb8a58d7223a38d9144bfaf25344
Reinhard <deuxexmachina1973@gmail.com>
2026-08-02 15:15:08 +0300

    Change first task logic.

--- src/task2_1/firsttask/StudentProfile.java
+++ src/task2_1/firsttask/StudentProfile.java
@@ -1,26 +1,31 @@
 package task2_1.firsttask;
 
 public class StudentProfile {
+    byte mark = 5;
+    short studentTicket = 127;
+    int birthDate = 1042005;
+    long phoneNumber = 79201242692L;
+    float averageMark = 4.5f;
+    double finalMark = 4.642;
+    boolean isPayment = true;
+    char firstLetterOfName = 'Д';
+
+    @Override
+    public String toString() {
+        return "StudentProfile{" +
+                "mark=" + mark +
+                ", studentTicket=" + studentTicket +
+                ", birthDate=" + birthDate +
+                ", phoneNumber=" + phoneNumber +
+                ", averageMark=" + averageMark +
+                ", finalMark=" + finalMark +
+                ", isPayment=" + isPayment +
+                ", firstLetterOfName=" + firstLetterOfName +
+                '}';
+    }
 
     public static void main(String[] args) {
-        byte mark = 5;
-        short studentTicket = 127;
-        int birthDate = 1042005;
-        long phoneNumber = 79201242692L;
-        float averageMark = 4.5f;
-        double finalMark = 4.642;
-        boolean isPayment = true;
-        char firstLetterOfName = 'Д';
-
-        System.out.println(
-                "Оценка: " + mark
-                + "\nНомер студенческого билета: " + studentTicket
-                + "\nДата рождения: " + birthDate
-                + "\nНомер телефона: " + phoneNumber
-                + "\nСредняя оценка: " + averageMark
-                + "\nИтоговая оценка: " + finalMark
-                + "\nСтатус оплаты: " + isPayment
-                + "\nПервая буква имени: " + firstLetterOfName
-        );
+        StudentProfile studentProfile = new StudentProfile();
+        System.out.println(studentProfile.toString());
     }
 }
