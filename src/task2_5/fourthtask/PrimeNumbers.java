07582d2dac0e6b447bd54689fb0ba64257a9b27f
Reinhard <deuxexmachina1973@gmail.com>
2026-08-02 15:06:11 +0300

    Rename path

---
+++ src/task2_5/fourthtask/PrimeNumbers.java
@@ -0,0 +1,28 @@
+package task2_5.fourthtask;
+
+public class PrimeNumbers {
+
+    public static void main(String[] args) {
+        int count = 0;
+
+        for (int i = 2; i <= 100; i++) {
+            boolean isPrime = true;
+
+            for (int j = 2; j <= Math.sqrt(i); j++) {
+                if (i % j == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            if (isPrime){
+                count++;
+                System.out.print(i);
+                System.out.print(" ");
+            }
+        }
+
+        System.out.println();
+        System.out.println("Всего простых чисел: " + count);
+    }
+}
